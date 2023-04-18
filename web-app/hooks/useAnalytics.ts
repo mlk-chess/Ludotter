b1a6d868e4746e46f19f4fc535fbeba69ac420e9
Théo Sigaud <sigaudtheo@gmail.com>
2023-04-18 19:50:00 +0200

    Add uuid analytics

--- web-app/hooks/useAnalytics.ts
+++ web-app/hooks/useAnalytics.ts
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 interface UserData {
+    id: string | null;
     ip: string;
     language: string;
+    userAgent: string;
 }
 
 const useAnalytics = (): UserData | null => {
@@ -14,7 +17,17 @@ const useAnalytics = (): UserData | null => {
                 const response = await fetch("https://api.ipify.org?format=json");
                 const data = await response.json();
                 const language = navigator.language;
-                setUserData({ ip: data.ip, language: language });
+                const userAgent = navigator.userAgent;
+
+                let id: string | null = localStorage.getItem('userId'); // récupération du cookie "userId" s'il existe
+                if (!id) {
+                    id = uuidv4();
+                    if (typeof id === "string") {
+                        localStorage.setItem('userId', id);
+                    }
+                }
+
+                setUserData({ ip: data.ip, language: language, userAgent: userAgent, id: id });
             } catch (error) {
                 console.error(error);
             }
