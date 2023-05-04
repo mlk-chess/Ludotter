828be5b7f2d7b57b0c46d5652bc0b27b51ca8597
waseem1104 <waseem11@hotmail.fr>
2023-05-05 00:53:35 +0200

    Front - Add loader done

--- web-app/pages/register.tsx
+++ web-app/pages/register.tsx
@@ -1,12 +1,51 @@
 import Head from 'next/head'
-import React, {useEffect} from "react";
+import React, {useEffect,useCallback,useState} from "react";
 import Link from "next/link";
+import { ClipLoader } from "react-spinners";
 
 export default function Register() {
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [lastname,setLastname] = useState('toto');
+    const [firstname,setFirstname] = useState('tta');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() =>
     {
         document.body.classList.add("bg-custom-light-orange");
-    });
+        
+    },[]);
+
+    const register = useCallback( async (e:any) => {
+
+        e.preventDefault();
+
+        if (password === confirmPassword) {
+
+            setIsLoading(true);
+            await fetch(`http://localhost:3001/register`,{
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email:email,password:password,lastname:lastname,firstname:firstname})
+            })
+            .then(response => response.json())
+            .then( (data) => {
+                console.log(data.message);
+                setIsLoading(false);
+            }).catch( (error) =>{
+                console.log(error);
+                setIsLoading(false);
+            });
+                
+            
+        }
+
+    },[email,password,confirmPassword,lastname,firstname])
+
 
     return (
         <>
@@ -20,21 +59,21 @@ export default function Register() {
                 <div className="grid h-screen place-items-center">
                     <div
                         className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                        <form className="space-y-6" action="#">
+                        <form className="space-y-6" onSubmit={register}>
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
                                     <label htmlFor="firstname"
                                            className="block mb-2 text-sm font-medium text-gray-900">Prénom</label>
                                     <input type="text" name="firstname" id="firstname"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                           placeholder="Jean" required/>
+                                           placeholder="Jean"/>
                                 </div>
                                 <div>
                                     <label htmlFor="name"
                                            className="block mb-2 text-sm font-medium text-gray-900">Nom</label>
                                     <input type="name" name="name" id="name" placeholder="Pierre"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                           required/>
+                                           />
                                 </div>
                             </div>
                             <div>
@@ -42,7 +81,7 @@ export default function Register() {
                                        className="block mb-2 text-sm font-medium text-gray-900">Email</label>
                                 <input type="email" name="email" id="email"
                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                       placeholder="email@exemple.com" required/>
+                                       placeholder="email@exemple.com" required onChange={ (e) => setEmail(e.target.value)} />
                             </div>
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
@@ -50,18 +89,18 @@ export default function Register() {
                                            className="block mb-2 text-sm font-medium text-gray-900">Mot de passe</label>
                                     <input type="password" name="password" id="password"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                           placeholder="******" required/>
+                                           placeholder="******"   onChange={ (e) => setPassword(e.target.value)} />
                                 </div>
                                 <div>
                                     <label htmlFor="confirmPassword"
                                            className="block mb-2 text-sm font-medium text-gray-900">Confirmation du mot de passe</label>
                                     <input type="password" name="confirmPassword" id="confirmPassword" placeholder="******"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                           required/>
+                                            onChange={ (e) => setConfirmPassword(e.target.value)}/>
                                 </div>
                             </div>
                             <button type="submit"
-                                    className="w-full text-white bg-custom-orange hover:bg-custom-hover-orange focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">S'inscrire
+                                    className="w-full text-white bg-custom-orange hover:bg-custom-hover-orange focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"> {isLoading ? <ClipLoader size={20} color={"#ffffff"} /> : "S'inscrire"}
                             </button>
                         </form>
                         <div className="text-sm font-medium text-gray-500 dark:text-gray-300 mt-10">
