29cf09678d7c7f95bd4fc379933d567903f7075b
waseem1104 <waseem11@hotmail.fr>
2023-05-08 22:42:09 +0200

    Fix design homepage

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
                 <h1 className={'text-orange-700 text-9xl mt-24'}>Page d'accueil</h1>
             </main> */}
             <HomeLayout>
-                <section>
+                <section className="bg-custom-light-orange">
                     <div className="container mx-auto py-12">
                         <div className="flex flex-col items-center md:flex-row">
                             <div className="text-center mb-12 md:text-left md:w-2/3 md:pr-10">
@@ -81,38 +81,38 @@ export default function Home() {
                         </div>
                     </div>
                 </section>
-                <section>
+                <section className="bg-custom-light-orange">
                     <div className="container mx-auto py-12">
-                        <div className="flex flex-col items-center md:flex-row">
+                        <div className="flex flex-col md:flex-row">
                             <div className="text-center mb-12 md:text-left md:w-1/3 md:pr-10">
                                 <h3 className="mb-8 font-medium md:text-lg xl:text-3xl">Avec LudOtter, vendez, louez et gagnez des cadeaux !</h3>
                             </div>
-                            <div className="flex flex-row md:w-2/3">
-                                <div className="flex-auto max-w-xs h-96 rounded-3xl overflow-hidden shadow-lg">
+                            <div className="md:flex md:flex-row md:w-2/3">
+                                <div className="bg-white py-10 mx-auto md:w-1/3 max-w-xs h-96 rounded-3xl shadow-lg">
                                     <img className="mx-auto" src="./dollar.png" alt=""/>
                                     <div className="my-14">
-                                        <div className="text-center	font-bold text-xl mb-2">Vendez</div>
+                                        <div className="text-center font-bold text-xl">Vendez</div>
                                     </div>
                                 </div>
-                                <div className="flex-auto mx-12 my-40 max-w-xs h-96 rounded-3xl overflow-hidden shadow-lg">
+                                <div className="bg-white mx-auto my-10 py-10 md:w-1/3 md:mx-12 md:mt-40 max-w-xs h-96 rounded-3xl shadow-lg">
                                     <img className="mx-auto" src="./fire.png" alt=""/>
                                     <div className="my-14">
-                                        <div className="text-center	font-bold text-xl mb-2">Gagnez</div>
+                                        <div className="text-center	font-bold text-xl">Gagnez</div>
                                     </div>
                                 </div>
-                                <div className="flex-auto max-w-xs h-96 rounded-3xl overflow-hidden shadow-lg">
+                                <div className="bg-white mx-auto py-10 md:w-1/3 max-w-xs h-96 rounded-3xl shadow-lg">
                                     <img className="mx-auto" src="./megaphone.png" alt=""/>
                                     <div className="my-14">
-                                        <div className="text-center	font-bold text-xl mb-2">Louez</div>
+                                        <div className="text-center	font-bold text-xl">Louez</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
-                <section>
+                <section className="bg-custom-light-orange">
                     <div className="container mx-auto relative">
-                    <div className="absolute top-0 right-0"> <img src="./rocket.png" width={200} alt=""/></div>
+                        <img className="absolute top-0 right-0" src="./rocket.png" width={200} alt=""/> 
                         <div className="w-full bg-custom-dark rounded-lg py-12 px-12">
                             <p className="mb-4 md:text-2xl text-white">Inscrivez-vous maintenant et rejoignez notre communauté de passionnés de jeux de société !</p>
                             <button
