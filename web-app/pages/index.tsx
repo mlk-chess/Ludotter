01625f5300e536cd8f1f3c2d4cfec5c5dcd806a6
jehanebnj <jehanebnj@gmail.com>
2023-05-08 18:17:12 +0200

    init home page design: first section

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -68,7 +68,23 @@ export default function Home() {
             {/* <main>
                 <h1 className={'text-orange-700 text-9xl mt-24'}>Page d'accueil</h1>
             </main> */}
-            <HomeLayout><h1>Test</h1></HomeLayout>
+            <HomeLayout>
+                <section>
+                    <div className="container mx-auto py-12">
+                        <div className="flex flex-col items-center md:flex-row">
+                            <div className="text-center mb-12 md:text-left md:w-2/3 md:pr-10">
+                                <h1 className="mb-8 font-medium md:text-3xl xl:text-5xl">Louez, achetez, rejoignez <br/> des parties de jeux de société  !</h1>
+                                <button
+                                    className="text-white bg-custom-orange hover:bg-custom-hover-orange focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">S'inscrire
+                                </button>
+                            </div>
+                            <div className="hidden md:block md:w-1/3">
+                                <img src="./crown.png" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </HomeLayout>
         </>
 )
 }
