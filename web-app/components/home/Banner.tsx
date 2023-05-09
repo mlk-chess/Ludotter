3d8511183875883a2913f9e5d754b9f93d4b3994
waseem1104 <waseem11@hotmail.fr>
2023-05-09 23:10:35 +0200

    Fix design homepage

--- web-app/components/home/Banner.tsx
+++ web-app/components/home/Banner.tsx
@@ -2,8 +2,8 @@ export default function Banner() {
 
     return (
         <section className="bg-custom-light-orange">
-            <div className="container mx-auto relative">
-                <img className="absolute top-0 right-0" src="./rocket.png" width={200} alt=""/> 
+            <div className="container mx-auto relative pr-10">
+                <img className="rocket absolute top-0 right-0" src="./rocket.png" width={175} alt=""/> 
                     <div className="w-full bg-custom-dark rounded-lg py-12 px-12">
                         <p className="mb-4 md:text-2xl text-white">Inscrivez-vous maintenant et rejoignez notre communauté de passionnés de jeux de société !</p>
                         <button
--- web-app/components/home/Header.tsx
+++ web-app/components/home/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 
     return (
         <section className="bg-custom-light-orange">
-            <div className="container mx-auto py-12">
+            <div className="container mx-auto py-12 h-3/4">
                 <div className="flex flex-col items-center md:flex-row">
                     <div className="text-center mb-12 md:text-left md:w-2/3 md:pr-10">
                         <h1 className="mb-8 font-semibold md:text-3xl xl:text-5xl"><span
