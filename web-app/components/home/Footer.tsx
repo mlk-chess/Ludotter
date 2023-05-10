96c257ca579685b2b485011460c2b1fdd4e4cb8b
jehanebnj <jehanebnj@gmail.com>
2023-05-10 08:46:38 +0200

    Translate and update the footer

--- web-app/components/home/Footer.tsx
+++ web-app/components/home/Footer.tsx
@@ -8,23 +8,23 @@ export default function Footer() {
                     <a href="#" className="flex items-center mb-4 sm:mb-0">
                         <img src="./otter.png" alt="logo" className="w-20 h-20 hover:duration-1000 hover:rotate-[360deg]"/>
                     </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
                         <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                            <a href="#" className="mr-4 hover:underline md:mr-6 ">À propos</a>
                         </li>
                         <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                            <a href="#" className="mr-4 hover:underline md:mr-6">Mentions légales</a>
                         </li>
                         <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                            <a href="#" className="mr-4 hover:underline md:mr-6 ">Conditions d'utilisations</a>
                         </li>
                         <li>
                             <a href="#" className="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center">© 2023 <a href="#  " className="hover:underline">LudOtter™</a>. All Rights Reserved.</span>
+                <hr className="my-6 border-gray-400 sm:mx-auto lg:my-8" />
+                <span className="block text-sm text-gray-500 sm:text-center">© 2023 <a href="#" className="hover:underline">LudOtter™</a>. Tous Droits Réservés.</span>
             </div>
         </footer>
     )
