5778aba0bb20c610c68e546668759ec5b1a356d9
waseem1104 <waseem11@hotmail.fr>
2023-05-10 22:22:25 +0200

    Update controller in auth service

--- nest-app/auth/src/app.controller.ts
+++ nest-app/auth/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'hello' })
+  hello(input?: string): string {
+
+    return `Hello, ${input || 'there'}!`;
+    
   }
+
 }
