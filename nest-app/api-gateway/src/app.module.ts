ac624ea32919fe34d7808a66a4fe36b6a21efac8
waseem1104 <waseem11@hotmail.fr>
2023-05-10 22:35:24 +0200

    Update module in api-gateway

--- nest-app/api-gateway/src/app.module.ts
+++ nest-app/api-gateway/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+
+    provide: 'AUTH_SERVICE',
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) =>
+
+    ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: 8080,
+
+      },
+
+    }),
+
+  }],
+
 })
+
 export class AppModule {}
