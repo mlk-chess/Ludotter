715c57685bffe176c9f99627036144bd4aed36dd
waseem1104 <waseem11@hotmail.fr>
2023-05-12 00:02:30 +0200

    Implement Toast for success message - register

--- web-app/pages/register.tsx
+++ web-app/pages/register.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import React, {useEffect,useCallback,useState} from "react";
 import Link from "next/link";
 import { ClipLoader } from "react-spinners";
-import Datepicker from "tailwind-datepicker-react";
+import 'flowbite';
 
 export default function Register() {
 
@@ -13,7 +13,9 @@ export default function Register() {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
-    // Affichage des erreurs. 
+
+    const [success, setSuccess] = useState("");
+    const [error, setError] = useState("");
 
     useEffect(() =>
     {
@@ -37,7 +39,11 @@ export default function Register() {
             })
             .then(response => response.json())
             .then( (data) => {
-                console.log(data.message);
+                if (data.statusCode === 201){
+                    setSuccess("Created")
+                }else{
+                    setError(data.message)
+                }
                 setIsLoading(false);
             }).catch( (error) =>{
                 console.log(error);
@@ -60,10 +66,25 @@ export default function Register() {
             </Head>
             <main>
                 <div className="grid h-screen place-items-center">
+                    {
+                        success !== "" ? 
+                            <div id="toast-success" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
+                                <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg">
+                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                                    <span className="sr-only">Check icon</span>
+                                </div>
+                                <div className="ml-3 text-sm font-normal">{success}</div>
+                                <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8" data-dismiss-target="#toast-success" aria-label="Close">
+                                    <span className="sr-only">Close</span>
+                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </button>
+                            </div>
+                        : ""
+                    }   
 
-                  
+
                     <div
-                        className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                        className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
                         <form className="space-y-6" onSubmit={register}>
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
@@ -111,7 +132,7 @@ export default function Register() {
                                     className="w-full text-white bg-custom-orange hover:bg-custom-hover-orange focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"> {isLoading ? <ClipLoader size={20} color={"#ffffff"} /> : "S'inscrire"}
                             </button>
                         </form>
-                        <div className="text-sm font-medium text-gray-500 dark:text-gray-300 mt-10">
+                        <div className="text-sm font-medium text-gray-500 mt-10">
                             Vous avez déjà un compte ? <Link href="/login" className="text-custom-orange hover:underline">Se connecter</Link>
                         </div>
                     </div>
