39831cf3a965d2f8d5556fd904df8137a1c3a7dc
jehanebnj <jehanebnj@gmail.com>
2023-05-11 17:46:56 +0200

    fix height for the flyer

--- web-app/components/home/Slider.tsx
+++ web-app/components/home/Slider.tsx
@@ -4,18 +4,18 @@ export default function Slider() {
 
     return (
         <section>
-            <div className="pb-8 h-">
+            <div className="pb-8 h-[900px]">
                 <div className="flex flex-row py-8 pl-10">
                     <img className="h-20" src="./target.png"alt="..."/>
                     <h3 className="self-center pl-4 font-medium md:text-4xl">Événement à venir</h3>
                 </div>
-                <div className="text-end pb-6 pr-8">
+                <div className="flex justify-end pb-6 pr-8">
                     <button type="button"
-                            className="text-white border-2 border-custom-orange bg-custom-orange hover:bg-custom-hover-orange focus:outline-none font-medium rounded-lg text-sm md:text-base py-2 px-4 md:px-4 md:py-2 text-center mr-3 md:mr-0 md:mx-2 ">Voir tous nos événements
+                            className="justify-end text-white border-2 border-custom-orange bg-custom-orange hover:bg-custom-hover-orange focus:outline-none font-medium rounded-lg text-sm md:text-base py-2 px-4 md:px-4 md:py-2 text-center mr-3 md:mr-0 md:mx-2 ">Voir tous nos événements
                     </button>
                 </div>
                
-                <div className="sm:h-96 xl:h-96 2xl:h-96">
+                <div className="h-56 sm:h-64 xl:h-[550px] 2xl:h-[750px]">
                     <Carousel className="">
                         <img src="./flyer.png"alt="..."/>
                         <img src="./flyer.png"alt="..."/>
