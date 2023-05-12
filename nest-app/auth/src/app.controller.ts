b96d3bf905345fae34ded7460001380243c5ccbc
waseem1104 <waseem11@hotmail.fr>
2023-05-12 10:10:49 +0200

    Create RPC validation filter

--- nest-app/auth/src/app.controller.ts
+++ nest-app/auth/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { createUserDto } from './dto/create-user.dto';
+import { RpcValidationFilter } from './filters/rpc-exception.filter';
 
 @Controller()
 export class AppController {
@@ -11,6 +12,7 @@ export class AppController {
 
   @MessagePattern({ cmd: 'register' })
   @UsePipes(ValidationPipe)
+  @UseFilters(new RpcValidationFilter())
   register(newUser : createUserDto) {
     return this.appService.register(newUser);
   }
