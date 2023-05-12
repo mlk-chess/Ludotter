62aa22a5dce98105c4b171b16c9206460f834a04
waseem1104 <waseem11@hotmail.fr>
2023-05-12 10:15:56 +0200

    Change RcpException to HttpException

--- nest-app/auth/src/filters/rpc-exception.filter.ts
+++ nest-app/auth/src/filters/rpc-exception.filter.ts
@@ -4,6 +4,6 @@ import { RpcException } from "@nestjs/microservices";
 @Catch(HttpException)
 export class RpcValidationFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
-        return new RpcException(exception.getResponse())
+        return new HttpException(exception.getResponse(),exception.getStatus())
     }
 }
