6eef70b793630082642e874a5c9c196dd8fc8457
Théo Sigaud <sigaudtheo@gmail.com>
2023-05-13 19:27:18 +0200

    Update header mobile

--- web-app/components/home/Header.tsx
+++ web-app/components/home/Header.tsx
@@ -3,9 +3,9 @@ export default function Header() {
     return (
         <section>
             <div className="container mx-auto py-12 h-3/4 mb-20">
-                <div className="flex flex-col items-center md:flex-row">
+                <div className="flex flex-col-reverse items-center md:flex-row">
                     <div className="text-center mb-12 md:text-left md:w-2/3 md:pr-10">
-                        <h1 className="mb-8 font-semibold md:text-3xl xl:text-5xl"><span
+                        <h1 className="mb-8 font-semibold text-3xl xl:text-5xl"><span
                             className="leading-[140%]"><span
                             className="bg-custom-pastel-purple rounded-2xl px-3 py-1 leading-[100%] inline-block">Louer</span>, <span
                             className="bg-custom-pastel-blue rounded-2xl px-3 py-1 leading-[100%] inline-block">acheter</span>, <span
@@ -15,8 +15,8 @@ export default function Header() {
                             className="text-white bg-custom-orange hover:bg-custom-hover-orange focus:outline-none font-medium rounded-lg text-base px-5 py-2.5 text-center">S'inscrire
                         </button>
                     </div>
-                    <div className="hidden md:block md:w-1/3">
-                        <img className="w-3/4 rotate-[20deg]" src="./home/crown.svg" alt="crown"/>
+                    <div className="w-1/3 transform translate-x-1/4 mb-10 md:mb-0">
+                        <img className="w-9/12 md:w-3/4 rotate-[20deg]" src="./home/crown.svg" alt="crown"/>
                     </div>
                 </div>
             </div>
