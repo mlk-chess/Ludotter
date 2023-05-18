4f4c06e1a76d93af93e75bdd64188fd16df122ce
Théo Sigaud <sigaudtheo@gmail.com>
2023-05-19 01:30:26 +0200

    Add style upload image

--- web-app/components/announcements/MultiImageUpload.tsx
+++ web-app/components/announcements/MultiImageUpload.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
 
 interface ImagePreview {
     id: string;
@@ -12,6 +12,13 @@ interface Props {
 }
 
 const MultiImageUpload: React.FC<Props> = ({ selectedImages, setSelectedImages }) => {
+    useEffect(() => {
+        const fileInput = document.getElementById('multi-image-upload') as HTMLInputElement | null;
+        if (fileInput) {
+            fileInput.value = '';
+        }
+    }, [selectedImages]);
+
     const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
         if (!files) return;
@@ -46,31 +53,57 @@ const MultiImageUpload: React.FC<Props> = ({ selectedImages, setSelectedImages }
         );
     };
 
+    const handleButtonClick = () => {
+        document.getElementById("multi-image-upload")?.click();
+    };
+
     return (
         <div>
+            <div className="border-dashed rounded border-2 border-gray-300 py-12 px-3 flex flex-col justify-center items-center mt-10">
+                <div className="mt-4 grid gap-4 grid-cols-2 md:grid-cols-4">
+                    {selectedImages.map((image) => (
+                        <div key={image.id} className="relative">
+                            <img
+                                src={image.previewUrl}
+                                alt="Preview"
+                                className="w-full rounded h-32 object-cover"
+                            />
+                            <button
+                                className="absolute top-2 right-2 py-0.5 px-3 bg-white text-black font-semibold rounded hover:bg-gray-200"
+                                onClick={() => handleRemoveImage(image.id)}
+                            >
+                                X
+                            </button>
+                        </div>
+                    ))}
+                    {(selectedImages.length > 0 && selectedImages.length < 4) &&
+                        <div className="h-full flex items-center ml-10">
+                            <button onClick={handleButtonClick} type="button" className="text-custom-dark bg-custom-white border-2 border-custom-highlight-orange hover:bg-custom-light-orange focus:outline-none font-medium rounded-lg text-sm lg:text-base py-2 px-4 md:py-2 text-center mr-0 w-fit h-fit">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                     stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                                </svg>
+                            </button>
+                        </div>
+                    }
+                </div>
+                {selectedImages.length === 0 &&
+                    <button onClick={handleButtonClick} type="button" className="text-custom-dark bg-custom-white border-2 border-custom-highlight-orange hover:bg-custom-light-orange focus:outline-none font-medium rounded-lg text-sm lg:text-base py-2 px-4 md:py-2 text-center mr-0">Ajoute des photos</button>
+                }
+                {selectedImages.length > 3  &&
+                    <div className="w-full flex justify-center mt-10">
+                        <p className="full">Vous ne pouvez pas ajouter plus d'images</p>
+                    </div>
+                }
+            </div>
             <input
                 type="file"
                 multiple
                 accept="image/*"
                 onChange={handleImageChange}
+                className="hidden"
+                id="multi-image-upload"
             />
-            <div className="mt-4 grid gap-4 grid-cols-2 md:grid-cols-4">
-                {selectedImages.map((image) => (
-                    <div key={image.id} className="relative">
-                        <img
-                            src={image.previewUrl}
-                            alt="Preview"
-                            className="w-full h-auto"
-                        />
-                        <button
-                            className="absolute top-2 right-2 p-1 bg-red-500 text-white rounded"
-                            onClick={() => handleRemoveImage(image.id)}
-                        >
-                            X
-                        </button>
-                    </div>
-                ))}
-            </div>
         </div>
     );
 };
