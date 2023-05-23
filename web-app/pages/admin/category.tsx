9af511ca1cd62f6c98d74dfd7278896668a877a2
waseem1104 <waseem11@hotmail.fr>
2023-05-23 23:41:02 +0200

    Add delete modal - category

--- web-app/pages/admin/category.tsx
+++ web-app/pages/admin/category.tsx
@@ -18,6 +18,7 @@ export default function Category() {
     const [error, setError] = useState("");
     const [showModal, setShowModal] = useState(false);
     const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [categorySelected, setCategorySelected] = useState<Category | undefined>(undefined);
 
     useEffect( () => {
@@ -100,7 +101,11 @@ export default function Category() {
     const openUpdateModal = useCallback( async (category:Category) => {
         setShowUpdateModal(true);
         setCategorySelected(category);
-        setName(category.name)
+    },[])
+
+    const openDeleteModal = useCallback( async (category:Category) => {
+        setShowDeleteModal(true);
+        setCategorySelected(category);
     },[])
 
     return (
@@ -129,6 +134,19 @@ export default function Category() {
                                         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Enregistrer</button>
                                     </div>
                                 </form>
+                            </Modal>
+                            </>
+                        ) : null}
+
+                        {showDeleteModal ? (
+                            <>
+                            <Modal setShowModal={setShowDeleteModal}>
+                                <h3 className="mb-5 text-lg font-normal text-gray-500">Voulez-vous vraiment supprimer cette catégorie ?</h3>
+                                <div className="flex justify-end">
+                                    <button type="button" className="text-white bg-red-600 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">
+                                        Supprimer
+                                    </button>
+                                </div>
                             </Modal>
                             </>
                         ) : null}
@@ -185,7 +203,7 @@ export default function Category() {
                         : ""
                     }   
                         <div className="flex justify-end">
-                            <button  onClick={() => setShowModal(true)} className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Créer une catégorie</button>
+                            <button onClick={() => setShowModal(true)} className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Créer une catégorie</button>
                         </div>
 
                         <div className="relative overflow-x-auto mt-5">
@@ -211,10 +229,14 @@ export default function Category() {
                                                    {category.name}
                                                 </td>
                                             
-                                                <td className="px-6 py-3">
+                                                <td className="px-6 py-3 flex">
 
-                                                    <svg onClick={ () => openUpdateModal(category)}  className="w-6 h-6 stroke-blue-500 cursor-pointer" fill="none" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                    <svg onClick={ () => openUpdateModal(category)} className="w-6 h-6 stroke-blue-500 cursor-pointer" fill="none" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                    </svg>
+
+                                                    <svg onClick={ () => openDeleteModal(category)}  fill="none" className="w-6 h-6 stroke-red-500 cursor-pointer" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                     </svg>
                                                 </td>
                                             </tr>
