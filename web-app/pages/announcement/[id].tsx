8d7b85d0c22ffff8abb2463a63b92e9b28b2a012
Théo Sigaud <sigaudtheo@gmail.com>
2023-05-27 17:08:54 +0200

    Add modal delete

--- web-app/pages/announcement/[id].tsx
+++ web-app/pages/announcement/[id].tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react";
 import HomeLayout from "@/components/layouts/Home";
 import {useRouter} from "next/router";
 import DisplayImages from "@/components/announcement/DisplayImages";
+import {Button, Modal} from "flowbite-react";
 
 interface Announcement {
     name: string;
@@ -61,7 +62,7 @@ export default function Announcement() {
                         {announcement.length > 0 &&
                             <div className="grid grid-cols-1 md:grid-cols-12 h-4/6">
                                 <DisplayImages images={announcement[0].base64Images}/>
-                                <div className="md:col-span-7 md:col-start-7 mt-10 md:mt-10">
+                                <div className="md:col-span-7 md:col-start-7 mt-10 md:mt-10 relative">
                                     <h2 className="mb-2 font-semibold leading-none text-gray-900 text-5xl">{announcement[0].name}</h2>
                                     <dl className="mt-16">
                                         <dt className="mb-2 font-semibold leading-none text-gray-900 text-2xl">Description</dt>
@@ -69,12 +70,8 @@ export default function Announcement() {
                                     </dl>
                                     <div className="flex justify-between">
                                         <div className="flex flex-col">
-                                            <p>Type de l'annonce :</p>
-                                            <p className="capitalize">{announcement[0].type}</p>
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <p>Catégories</p>
-                                            <div className="py-5">
+                                            <p className="font-semibold">Catégories :</p>
+                                            <div className="py-4">
                                                 {announcement[0].announcementCategories.map((item, index) => {
                                                     return (
                                                         <span key={index}
@@ -86,51 +83,57 @@ export default function Announcement() {
                                             </div>
                                         </div>
 
-                                        <button onClick={() => setDeleteModal(true)}
-                                                className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                                type="button">
-                                            Toggle modal
-                                        </button>
+                                        <div className="flex flex-col">
+                                            <p className="font-semibold">Type de l'annonce :</p>
+                                            <p className="capitalize py-4">{announcement[0].type}</p>
+                                        </div>
                                     </div>
+
+                                    <div className="absolute bottom-0 left-0">
+                                        <Button color="failure" onClick={() => setDeleteModal(true)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-3">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                            </svg>
+
+                                            Supprimer l'annonce
+                                        </Button>
+                                    </div>
+                                    <Modal
+                                        onClose={() => setDeleteModal(false)}
+                                        show={deleteModal}
+                                        popup
+                                        size="md"
+                                    >
+                                        <Modal.Header />
+                                        <Modal.Body>
+                                            <div className="text-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                                </svg>
+
+                                                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                                    Voulez-vous vraiment supprimer cette annonce ?
+                                                </h3>
+                                                <div className="flex justify-center gap-4">
+                                                    <Button
+                                                        color="failure"
+                                                        onClick={() => setDeleteModal(false)}
+                                                    >
+                                                        Oui, je suis sûr
+                                                    </Button>
+                                                    <Button
+                                                        color="gray"
+                                                        onClick={() => setDeleteModal(false)}
+                                                    >
+                                                        Non, annuler
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </Modal.Body>
+                                    </Modal>
                                 </div>
                             </div>
                         }
-                        <div className={`${deleteModal ? 'block' : 'hidden'} fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-                            <div className="relative w-full max-w-md max-h-full">
-                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                    <button type="button"
-                                            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                                            data-modal-hide="popup-modal">
-                                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor"
-                                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                  clip-rule="evenodd"></path>
-                                        </svg>
-                                        <span className="sr-only">Close modal</span>
-                                    </button>
-                                    <div className="p-6 text-center">
-                                        <svg aria-hidden="true"
-                                             className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
-                                             fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                             xmlns="http://www.w3.org/2000/svg">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                        </svg>
-                                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are
-                                            you sure you want to delete this product?</h3>
-                                        <button data-modal-hide="popup-modal" type="button"
-                                                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                            Yes, I'm sure
-                                        </button>
-                                        <button data-modal-hide="popup-modal" type="button"
-                                                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                                            cancel
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </section>
             </HomeLayout>
