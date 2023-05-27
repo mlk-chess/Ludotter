45004ecfe65bb53fb6fa57f5a6b19c7c2f18f317
Théo Sigaud <sigaudtheo@gmail.com>
2023-05-27 19:12:30 +0200

    Add loader

--- web-app/components/announcement/FormCreate.tsx
+++ web-app/components/announcement/FormCreate.tsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useEffect, useState} from "react";
 import MultiImageUpload from "@/components/announcement/MultiImageUpload";
+import {useRouter} from "next/router";
 
 interface ImagePreview {
     id: string;
@@ -24,6 +25,8 @@ export default function FormCreate() {
     const [description, setDescription] = useState("");
     const [success, setSuccess] = useState("");
     const [error, setError] = useState("");
+    const [isSave, setIsSave] = useState<boolean>(false);
+    const router = useRouter();
 
     const convertImageToBase64 = ({file}: { file: any }) => {
         return new Promise((resolve, reject) => {
@@ -35,7 +38,7 @@ export default function FormCreate() {
     };
 
     const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value, checked } = event.target;
+        const {value, checked} = event.target;
         if (checked) {
             setSelectCategories((prevCheckedBoxes) => [...prevCheckedBoxes, value]);
         } else {
@@ -73,6 +76,8 @@ export default function FormCreate() {
     const save = useCallback(async (e: any) => {
         e.preventDefault();
 
+        setIsSave(true);
+
         const selectedImagesBase64 = await Promise.all(
             selectedImages.map(async (image) => ({
                 name: image.file.name,
@@ -100,8 +105,9 @@ export default function FormCreate() {
             .then((data) => {
 
                 if (data.statusCode === 201) {
-                    setSuccess("Created.")
-                    setError("")
+                    setSuccess("Created.");
+                    setError("");
+                    router.push('/announcement');
                 } else {
                     setError(data.response.message)
                     setSuccess("")
@@ -145,7 +151,8 @@ export default function FormCreate() {
                              className="z-10 hidden bg-white rounded-lg shadow w-full p-2.5 overflow-y-auto max-h-72">
                             {categories.map((item, index) => (
                                 <div className="flex items-ce nter mb-4" key={index}>
-                                    <input onChange={handleCheckboxChange} id={`${item.name}-checkbox`} type="checkbox" value={item.id}
+                                    <input onChange={handleCheckboxChange} id={`${item.name}-checkbox`} type="checkbox"
+                                           value={item.id}
                                            className="w-4 h-4 text-custom-pastel-purple bg-gray-100 border-gray-300 rounded focus:ring-custom-pastel-purple"/>
                                     <label htmlFor={`${item.name}-checkbox`}
                                            className="ml-2 text-sm font-medium text-gray-900">{item.name}</label>
@@ -158,7 +165,8 @@ export default function FormCreate() {
                                 const name = category ? category.name : "";
 
                                 return (
-                                    <span key={index} className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-400 text-white mr-2">
+                                    <span key={index}
+                                          className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-400 text-white mr-2">
                                         {name}
                                     </span>
                                 );
@@ -172,7 +180,8 @@ export default function FormCreate() {
 
                         <div className="flex pt-3">
                             <div className="flex items-between mr-4">
-                                <input defaultChecked onChange={(e) => setType(e.target.value)} id="location" type="radio"
+                                <input defaultChecked onChange={(e) => setType(e.target.value)} id="location"
+                                       type="radio"
                                        value="location" name="inline-radio-group-type" required
                                        className="w-4 h-4 text-custom-pastel-purple bg-gray-100 border-gray-300 focus:ring-custom-pastel-purple focus:ring-2"/>
                                 <label htmlFor="location"
@@ -218,10 +227,24 @@ export default function FormCreate() {
                     </div>
                 </div>
                 <MultiImageUpload selectedImages={selectedImages} setSelectedImages={setSelectedImages}/>
-                <button type="submit"
-                        className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-custom-orange rounded-lg hover:bg-custom-hover-orange">
-                    Poster
-                </button>
+                {isSave ?
+                    <svg aria-hidden="true"
+                         className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                         viewBox="0 0 100 101" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"/>
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"/>
+                    </svg>
+                    :
+                    <button type="submit"
+                            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-custom-orange rounded-lg hover:bg-custom-hover-orange">
+                        Poster
+                    </button>
+                }
             </form>
         </div>
     )
--- web-app/pages/announcement/[id].tsx
+++ web-app/pages/announcement/[id].tsx
@@ -26,6 +26,7 @@ interface Category {
 export default function Announcement() {
     const [announcement, setAnnouncement] = useState<Announcement[]>([]);
     const [deleteModal, setDeleteModal] = useState<boolean>(false);
+    const [isDelete, setIsDelete] = useState<boolean>(false);
     const [idAnnouncement, setIdAnnouncement] = useState<string>('');
     const [error, setError] = useState("");
     const router = useRouter();
@@ -37,7 +38,7 @@ export default function Announcement() {
     useEffect(() => {
         if (!router.isReady) return;
 
-        const { id } = router.query;
+        const {id} = router.query;
         if (typeof id === 'string') {
             setIdAnnouncement(id);
 
@@ -56,6 +57,8 @@ export default function Announcement() {
 
     const deleteAnnouncement = useCallback(async (e: any) => {
         e.preventDefault();
+
+        setIsDelete(true);
 
         await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/announcement/delete`, {
             method: 'DELETE',
@@ -118,8 +121,10 @@ export default function Announcement() {
 
                                     <div className="2xl:absolute bottom-0 left-0">
                                         <Button color="failure" onClick={() => setDeleteModal(true)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-3">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-3">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
                                             </svg>
 
                                             Supprimer l'annonce
@@ -131,30 +136,47 @@ export default function Announcement() {
                                         popup
                                         size="md"
                                     >
-                                        <Modal.Header />
+                                        <Modal.Header/>
                                         <Modal.Body>
                                             <div className="text-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mx-auto mb-4 h-14 w-14 text-gray-400">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     strokeWidth={1.5} stroke="currentColor"
+                                                     className="mx-auto mb-4 h-14 w-14 text-gray-400">
+                                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                                          d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"/>
                                                 </svg>
 
                                                 <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
                                                     Voulez-vous vraiment supprimer cette annonce ?
                                                 </h3>
-                                                <div className="flex justify-center gap-4">
-                                                    <Button
-                                                        color="failure"
-                                                        onClick={deleteAnnouncement}
-                                                    >
-                                                        Oui, je suis sûr
-                                                    </Button>
-                                                    <Button
-                                                        color="gray"
-                                                        onClick={() => setDeleteModal(false)}
-                                                    >
-                                                        Non, annuler
-                                                    </Button>
-                                                </div>
+                                                {isDelete ?
+                                                    <svg aria-hidden="true"
+                                                         className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                                                         viewBox="0 0 100 101" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+                                                        <path
+                                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                            fill="currentColor"/>
+                                                        <path
+                                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                            fill="currentFill"/>
+                                                    </svg>
+                                                    :
+                                                    <div className="flex justify-center gap-4">
+                                                        <Button
+                                                            color="failure"
+                                                            onClick={deleteAnnouncement}
+                                                        >
+                                                            Oui, je suis sûr
+                                                        </Button>
+                                                        <Button
+                                                            color="gray"
+                                                            onClick={() => setDeleteModal(false)}
+                                                        >
+                                                            Non, annuler
+                                                        </Button>
+                                                    </div>
+                                                }
                                             </div>
                                         </Modal.Body>
                                     </Modal>
