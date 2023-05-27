00f967f552fc2ed224f31c0dea11965969a3cc80
Théo Sigaud <sigaudtheo@gmail.com>
2023-05-27 19:24:08 +0200

    Delete dark css

--- web-app/components/announcement/FormCreate.tsx
+++ web-app/components/announcement/FormCreate.tsx
@@ -229,7 +229,7 @@ export default function FormCreate() {
                 <MultiImageUpload selectedImages={selectedImages} setSelectedImages={setSelectedImages}/>
                 {isSave ?
                     <svg aria-hidden="true"
-                         className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                         className="mt-4 inline w-8 h-8 mr-2 text-gray-200 animate-spin fill-gray-600"
                          viewBox="0 0 100 101" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
