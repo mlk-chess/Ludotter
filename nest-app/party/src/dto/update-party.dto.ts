27e1a41f28ccff7ba7231a9fa3028d713c2e8fed
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-04 15:35:12 +0200

    Update party with DTO

--- nest-app/party/src/dto/update-party.dto.ts
+++ nest-app/party/src/dto/update-party.dto.ts
@@ -1,11 +1,36 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsUUID, IsInt, Min, IsString, Matches } from 'class-validator';
 
 export class updatePartyDto {
-
-    @IsNumberString({}, {message:"Soirée invalide"})
+    
+    @IsUUID()
     id: string;
 
-    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    @IsNotEmpty({ message: "Veuillez remplir tous les champs." })
+    @IsString()
     name: string;
 
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    location?: string;
+
+    @IsOptional()
+    @IsInt()
+    @Min(2)
+    players?: number;
+
+    @IsOptional()
+    @IsUUID()
+    owner?: string;
+
+    @IsOptional()
+    @Matches(/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/)
+    time?: string;
+
+    constructor(data: Partial<updatePartyDto>) {
+        Object.assign(this, data);
+    }
 }
