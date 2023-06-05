f9b6872227775933edf03fff2d1a5a8971ff3e47
waseem1104 <waseem11@hotmail.fr>
2023-06-05 23:11:12 +0200

    Update DTO create event

--- nest-app/event/src/dto/create-event.dto.ts
+++ nest-app/event/src/dto/create-event.dto.ts
@@ -1,6 +1,12 @@
 import { IsNotEmpty, Length } from 'class-validator';
 
 export class createEventDto {
+
     @IsNotEmpty({message:"Veuillez remplir tous les champs."})
     name: string;
+
+    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    description: string;
+
+    
 }
