5f094d2f5fc52856d4ba54440c2aeaf399dd94b9
waseem1104 <waseem11@hotmail.fr>
2023-06-18 19:06:47 +0200

    Update event DTO

--- nest-app/event/src/dto/update-event.dto.ts
+++ nest-app/event/src/dto/update-event.dto.ts
@@ -1,11 +1,26 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, Length, IsDate, IsDateString, isInt, IsNumberString} from 'class-validator';
 
 export class updateEventDto {
 
-    @IsNumberString({}, {message:"Evènement invalide"})
-    id: string;
-
     @IsNotEmpty({message:"Veuillez remplir tous les champs."})
     name: string;
+
+    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    description: string;
+
+    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    @Transform( ({ value }) => value && new Date(value))
+    @IsDate()
+    date: Date;
+
+    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    time : string;
+
+    @IsNotEmpty({message:"Veuillez remplir tous les champs."})
+    players: number
+
+    @IsNumberString({}, {message:"Identifiant invalide"})
+    id: string;
 
 }
