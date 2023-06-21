a2abbe00a3e31f8a5fcdaaaf456f28ed0fed4559
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-21 16:56:19 +0200

    DTO for party

---
+++ nest-app/party/src/dto/join-party.dto.ts
@@ -0,0 +1,14 @@
+import { IsUUID } from 'class-validator';
+
+export class joinPartyDto {
+
+    @IsUUID()
+    partyId: string;
+
+    @IsUUID()
+    profileId: string;
+
+    constructor(data: Partial<joinPartyDto>) {
+        Object.assign(this, data);
+    }
+}
