15a033985caac3b0c1cec26654385ad7f9111c93
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-21 10:22:41 +0200

    Current page in evidence

--- web-app/pages/party/index.tsx
+++ web-app/pages/party/index.tsx
@@ -73,7 +73,7 @@ export default function New() {
                     </li>
                     {pageNumbers.map(number => (
                         <li key={number} className="page-item">
-                            <a onClick={() => handlePageChange(number)} href="#" className="page-lin px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <a onClick={() => handlePageChange(number)} href="#" className={`page-lin px-3 py-2 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage === number ? 'bg-white' : ''}`}>
                                 {number}
                             </a>
                         </li>
@@ -84,6 +84,7 @@ export default function New() {
                     </li>
                 </ul>
             </nav>
+
         );
     }
 
