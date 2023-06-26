fe374728687aebfbd12109b3c0a0c4a7f604f2bb
waseem1104 <waseem11@hotmail.fr>
2023-06-26 17:34:21 +0200

    Create component Message - Conversation and add interface

---
+++ web-app/components/message/Conversation.tsx
@@ -0,0 +1,90 @@
+import React, {Fragment, useEffect, useRef, useState} from "react";
+
+interface ConversationProps {
+    party: string;
+  }
+
+  const Conversation: React.FC<ConversationProps> = ({ party }) => {
+
+
+    useEffect( () => {
+       
+        
+    },[party])
+
+    return (
+
+        <div className="flex flex-col flex-auto h-full p-6">
+        <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-custom-highlight-orange shadow-sm h-full p-4">
+        <div className="flex flex-col h-full overflow-x-auto mb-4">
+            <div className="flex flex-col h-full">
+            <div className="grid grid-cols-12 gap-y-2">
+               
+                <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                <div className="flex flex-row items-center">
+                    
+                    <div
+                    className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                    >
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Vel ipsa commodi illum saepe numquam maxime
+                        asperiores voluptate sit, minima perspiciatis.
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                <div className="flex items-center justify-start flex-row-reverse">
+                    
+                    <div
+                    className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                    >
+                    <div>I'm ok what about you?</div>
+                    </div>
+                </div>
+                </div>
+               
+                
+        
+            </div>
+            </div>
+        </div>
+        <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+        
+            <div className="flex-grow ml-4">
+            <div className="relative w-full">
+                <input type="text" placeholder="Message..." className="flex w-full rounded-xl border-gray-300 pl-4 h-10"/>
+            </div>
+            </div>
+            <div className="ml-4">
+            <button className="flex items-center justify-center bg-custom-pastel-blue hover:bg-indigo-600 rounded-xl text-black font-medium px-4 py-1 flex-shrink-0">
+                <span>Envoyer</span>
+                <span className="ml-2">
+                <svg
+                    className="w-4 h-4 transform rotate-45 -mt-px"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                    ></path>
+                </svg>
+                </span>
+            </button>
+            </div>
+        </div>
+        </div>
+    </div>
+   
+
+    );
+
+}
+
+export default Conversation;
--- web-app/pages/message/index.tsx
+++ web-app/pages/message/index.tsx
@@ -2,12 +2,20 @@ import React, {Fragment, useEffect, useRef, useState} from "react";
 import HomeLayout from "@/components/layouts/Home";
 import Head from 'next/head'
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Conversation from "@/components/message/Conversation";
 
 
 export default function Message(){
 
+    interface Party{
+        name: string;
+        id: string;
+    }
+
     const supabaseClient = useSupabaseClient();
     const [parties,setParties] = useState([]);
+    const [party,setParty] = useState<string>("");
+
 
     useEffect(() => {
         document.body.classList.add("bg-custom-light-orange");
@@ -49,95 +57,27 @@ export default function Message(){
                     <div className="flex flex-row items-center justify-between text-xs">
                         <span className="font-bold">Annonces</span>
                     </div>
-                    <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-                        {
-                            parties.map((party, index) => (
-                                <button className="flex flex-row items-center hover:bg-custom-pastel-purple rounded-xl p-2">
-                                    <div className="ml-2 text-sm font-semibold">{party.name}</div>
-                                </button>
-                            ))
-                        }
-                       
-                    </div>
+                    
                     <div className="flex flex-row items-center justify-between text-xs mt-6">
                         <span className="font-bold">Soirées</span>
                         
                     </div>
-                    <div className="flex flex-col space-y-1 mt-4 -mx-2">
-                        <button className="flex flex-row items-center hover:bg-custom-pastel-purple rounded-xl p-2">
-                            <div className="ml-2 text-sm font-semibold">Henry Boyd</div>
-                        </button>
-                    </div>
-                    </div>
-                </div>
-                <div className="flex flex-col flex-auto h-full p-6">
-                    <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-custom-highlight-orange shadow-sm h-full p-4">
-                    <div className="flex flex-col h-full overflow-x-auto mb-4">
-                        <div className="flex flex-col h-full">
-                        <div className="grid grid-cols-12 gap-y-2">
-                           
-                            <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                            <div className="flex flex-row items-center">
-                                
-                                <div
-                                className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                                >
-                                <div>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing
-                                    elit. Vel ipsa commodi illum saepe numquam maxime
-                                    asperiores voluptate sit, minima perspiciatis.
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                            <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                            <div className="flex items-center justify-start flex-row-reverse">
-                                
-                                <div
-                                className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
-                                >
-                                <div>I'm ok what about you?</div>
-                                </div>
-                            </div>
-                            </div>
-                           
-                            
                     
-                        </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+                    <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
+                        {
+                            parties.map((party:Party, index) => (
+                                <button onClick={ () => setParty(party.id)} key={index} className="flex flex-row items-center hover:bg-custom-pastel-purple rounded-xl p-2">
+                                    <div className="ml-2 text-sm font-semibold">{party.name}</div>
+                                </button>
+                            ))
+                        }
                     
-                        <div className="flex-grow ml-4">
-                        <div className="relative w-full">
-                            <input type="text" placeholder="Message..." className="flex w-full rounded-xl border-gray-300 pl-4 h-10"/>
-                        </div>
-                        </div>
-                        <div className="ml-4">
-                        <button className="flex items-center justify-center bg-custom-pastel-blue hover:bg-indigo-600 rounded-xl text-black font-medium px-4 py-1 flex-shrink-0">
-                            <span>Envoyer</span>
-                            <span className="ml-2">
-                            <svg
-                                className="w-4 h-4 transform rotate-45 -mt-px"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                                ></path>
-                            </svg>
-                            </span>
-                        </button>
-                        </div>
                     </div>
                     </div>
                 </div>
-                </div>
+
+                <Conversation party={party} />
+              </div>
             </div>
                 
             </HomeLayout>
