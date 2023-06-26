53c68b320c1074a535ae50b3db8845dcae5c15a6
Théo Sigaud <sigaudtheo@gmail.com>
2023-06-26 12:13:16 +0200

    Add errors checkout

--- web-app/components/announcement/checkout.tsx
+++ web-app/components/announcement/checkout.tsx
@@ -2,6 +2,13 @@ import React, {useState} from "react";
 import Cards from 'react-credit-cards-2';
 import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
+interface Error {
+    number: string;
+    expiry: string;
+    cvc: string;
+    name: string;
+}
+
 export default function Checkout() {
     const [state, setState] = useState({
         number: '',
@@ -10,6 +17,10 @@ export default function Checkout() {
         name: '',
         focus: '',
     });
+    const [isCheckout, setIsCheckout] = useState<boolean>(false);
+    const [errorsCheckout, setErrorsCheckout] = useState<Error>({} as Error);
+    const [error, setError] = useState<boolean>(false);
+
 
     const handleInputChange = (evt) => {
         const {name, value} = evt.target;
@@ -55,22 +66,56 @@ export default function Checkout() {
     }
 
     const checkoutAnnouncement = () => {
-        fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/announcement/checkout`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id: '12',
-            })
-        })
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data)
-            }).catch((error) => {
-            console.log(error);
+        if (state.number.length < 16) {
+            setErrorsCheckout((prevState) => ({
+                ...prevState,
+                number: "Le numéro de la carte est invalide"
+            }));
+            setError(true);
+        }
 
-        });
+        if (state.name.length < 3) {
+            setErrorsCheckout((prevState) => ({
+                ...prevState,
+                name: "Le nom du titulaire doit contenir au moins 3 caractères"
+            }));
+            setError(true);
+        }
+
+        if (state.expiry.length < 5) {
+            setErrorsCheckout((prevState) => ({
+                ...prevState,
+                expiry: "La date d'expiration est invalide"
+            }));
+            setError(true);
+        }
+
+        if (state.cvc.length < 3) {
+            setErrorsCheckout((prevState) => ({
+                ...prevState,
+                cvc: "Le code de sécurité est invalide"
+            }));
+            setError(true);
+        }
+
+        if (!error) {
+            fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/announcement/checkout`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: '12',
+                })
+            })
+                .then(response => response.json())
+                .then((data) => {
+                    console.log(data)
+                }).catch((error) => {
+                console.log(error);
+
+            });
+        }
     }
 
     return (
@@ -82,7 +127,7 @@ export default function Checkout() {
                     cvc={state.cvc}
                     name={state.name}
                     focused={state.focus}
-                    locale={{valid: 'valide jusqu\'à'}}
+                    locale={{valid: 'valide jusqu\'au'}}
                     placeholders={{name: 'Nom du titulaire'}}
                 />
                 <div>
@@ -97,6 +142,7 @@ export default function Checkout() {
                                onChange={handleInputChange}
                                onFocus={handleInputFocus}
                                required/>
+                        <p className="text-red-600 text-sm">{errorsCheckout.number}</p>
                     </div>
 
                     <div className="mb-6">
@@ -109,6 +155,7 @@ export default function Checkout() {
                                onChange={handleInputChange}
                                onFocus={handleInputFocus}
                                required/>
+                        <p className="text-red-600 text-sm">{errorsCheckout.name}</p>
                     </div>
 
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
@@ -132,6 +179,7 @@ export default function Checkout() {
                                 <option value="11">11</option>
                                 <option value="12">12</option>
                             </select>
+                            <p className="text-red-600 text-sm">{errorsCheckout.expiry}</p>
                         </div>
 
                         <div>
@@ -169,14 +217,36 @@ export default function Checkout() {
                                        onChange={handleInputChange}
                                        onFocus={handleInputFocus}
                                        required/>
+                                <p className="text-red-600 text-sm">{errorsCheckout.cvc}</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button
-                    className="text-white border-2 border-custom-orange bg-custom-orange hover:bg-custom-hover-orange focus:outline-none font-medium rounded-lg text-base px-4 py-2 text-center mr-0 mx-10 "
-                    onClick={checkoutAnnouncement}>Acheter
-                </button>
+
+                {/*{errorsCheckout &&*/}
+                {/*    <p className="text-red-600">Erreur</p>*/}
+                {/*}*/}
+
+                {isCheckout ?
+                    <svg aria-hidden="true"
+                         className="mt-4 inline w-8 h-8 text-gray-200 animate-spin fill-gray-600"
+                         viewBox="0 0 100 101" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"/>
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"/>
+                    </svg>
+                    :
+                    <div className="flex justify-center">
+                        <button
+                            className="text-white border-2 border-custom-orange bg-custom-orange hover:bg-custom-hover-orange focus:outline-none font-medium rounded-lg text-base px-4 py-2 text-center"
+                            onClick={checkoutAnnouncement}>Acheter
+                        </button>
+                    </div>
+                }
             </div>
         </>
     )
