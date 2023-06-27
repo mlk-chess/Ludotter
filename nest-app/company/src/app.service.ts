32cbea1a5e91a60d1397ef7b435089d5df359cd1
jehanebnj <jehanebnj@gmail.com>
2023-06-27 15:51:58 +0200

    Init save company method in API gateway and microservice

--- nest-app/company/src/app.service.ts
+++ nest-app/company/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { createCompanyDto } from './dto/create-company.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async saveCompany(company: createCompanyDto){
+    return 'OK';
   }
 }
