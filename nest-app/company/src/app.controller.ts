24156fcfabf8c37756b8f0ebb68e61a802f7fea4
jehanebnj <jehanebnj@gmail.com>
2023-06-27 15:28:35 +0200

    Test microservice company - OK

--- nest-app/company/src/app.controller.ts
+++ nest-app/company/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @MessagePattern({ cmd: 'company_getCompanies' })
+  getCompanies() {
     return this.appService.getHello();
   }
+  
 }
