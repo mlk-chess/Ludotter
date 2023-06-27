c712489adaf1914bdfe4fc3cc43c3f12a2c7e0c6
jehanebnj <jehanebnj@gmail.com>
2023-06-27 16:22:10 +0200

    Insert company - Add verification - check if email is unique

--- nest-app/company/src/app.service.ts
+++ nest-app/company/src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { createCompanyDto } from './dto/create-company.dto';
 import { SupabaseService } from './supabase/supabase.service';
 
@@ -13,25 +13,42 @@ constructor(private supabaseService: SupabaseService) {}
   }
 
   async saveCompany(company: createCompanyDto){
+    let emailIsUnique = await this.checkIfEmailUnique(company.email);
+    if(emailIsUnique){
+      const { error } =  await this.supabaseService.client
+      .from('company')
+      .insert([
+        {
+          name: company.name,
+          email: company.email,
+          address: company.address,
+          city: company.city,
+          zipcode: company.zipcode,
+          message: company.message,
+          number: company.number
+        }
+      ]);
 
-    const { error } =  await this.supabaseService.client
-    .from('company')
-    .insert([
-      {
-         name: company.name,
-         email: company.email,
-         address: company.address,
-         city: company.city,
-         zipcode: company.zipcode,
-         message: company.message,
-         number: company.number
+      if (error) {
+          throw error;
       }
-    ]);
 
-    if (error) {
-        throw error;
+      return { statusCode : 201, message : "Created"}
     }
-
-    return { statusCode : 201, message : "Created"}
+    return new HttpException({message : ["L'email est déjà utilisé."]}, HttpStatus.BAD_REQUEST);
   }
+
+  async checkIfEmailUnique(email:string){
+
+      const { data: users, error: emailCheckError } = await this.supabaseService.client
+      .from('company')
+      .select('*')
+      .eq('email',email)
+
+      if (emailCheckError) {
+          throw emailCheckError;
+      }
+      return users.length === 0
+      
+    }
 }
