5bf1598af94de78a0191f1d346b44172ac4e49cf
jehanebnj <jehanebnj@gmail.com>
2023-06-27 16:15:15 +0200

    Insert company - nestjs

--- nest-app/company/src/app.service.ts
+++ nest-app/company/src/app.service.ts
@@ -1,13 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { createCompanyDto } from './dto/create-company.dto';
+import { SupabaseService } from './supabase/supabase.service';
 
 @Injectable()
 export class AppService {
+
+constructor(private supabaseService: SupabaseService) {}
+
+
   getHello(): string {
     return 'Hello World!';
   }
 
   async saveCompany(company: createCompanyDto){
-    return 'OK';
+
+    const { error } =  await this.supabaseService.client
+    .from('company')
+    .insert([
+      {
+         name: company.name,
+         email: company.email,
+         address: company.address,
+         city: company.city,
+         zipcode: company.zipcode,
+         message: company.message,
+         number: company.number
+      }
+    ]);
+
+    if (error) {
+        throw error;
+    }
+
+    return { statusCode : 201, message : "Created"}
   }
 }
