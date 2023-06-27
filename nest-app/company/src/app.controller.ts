2d71119bdf19e623d00472a639b89b31a71b1805
jehanebnj <jehanebnj@gmail.com>
2023-06-27 15:58:46 +0200

    Add UsePipes and fix class-validator

--- nest-app/company/src/app.controller.ts
+++ nest-app/company/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseFilters } from '@nestjs/common';
+import { Controller, Get, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern, RpcException } from '@nestjs/microservices';
 import { createCompanyDto } from './dto/create-company.dto';
@@ -14,6 +14,7 @@ export class AppController {
   }
 
   @MessagePattern({ cmd: 'company_saveCompany' })
+  @UsePipes(ValidationPipe)
   @UseFilters(new RpcValidationFilter())
   saveCompany(createCompany: createCompanyDto) {
     return this.appService.saveCompany(createCompany);
