34dcde4741d73a52252ffc2d539d154d582aeab2
jehanebnj <jehanebnj@gmail.com>
2023-06-29 01:12:16 +0200

    Test Auth guard in category - OK

--- nest-app/api-gateway/src/category/category.controller.ts
+++ nest-app/api-gateway/src/category/category.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post, Get, Inject, Param, Body, Patch, Delete, UseGuards, Req } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
 
 @Controller('category')
 export class CategoryController {
 
   constructor(@Inject('CATEGORY_SERVICE') private client: ClientProxy) {}
 
-
+  
+  
   @Get('')
+  @UseGuards(AuthGuard) 
   getCategories() {
     return this.client.send({ cmd: 'category_getCategories' },{});
   }
