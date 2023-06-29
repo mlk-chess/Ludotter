79e65c78da2bc1042c3aae3a6fd50faae1f1adb1
jehanebnj <jehanebnj@gmail.com>
2023-06-29 19:10:37 +0200

    Implement modal update event

--- web-app/pages/admin/event/index.tsx
+++ web-app/pages/admin/event/index.tsx
@@ -12,6 +12,7 @@ interface Event {
     time: string;
     company:Company;
     status:number;
+    players:number
 }
 
 interface Company{
@@ -123,6 +124,75 @@ export default function Event() {
                                         Supprimer
                                     </button>
                                 </div>
+                            </Modal>
+                            </>
+                        ) : null}
+
+
+                        {showUpdateModal ? (
+                            <>
+                            <Modal setShowModal={setShowUpdateModal} title="Modification">
+                            <form onSubmit={save}>
+                                <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                    <div className="w-full">
+                                        <label htmlFor="name"
+                                            className="block mb-2 text-sm font-medium text-gray-900">Nom</label>
+                                        <input value={eventSelected?.name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:bg-white block w-full p-2.5"
+                                            placeholder="Le nom de votre évènement" required/>
+                                    </div>
+
+
+                                    <div className="w-full">
+                                        <label htmlFor="players"
+                                            className="block mb-2 text-sm font-medium text-gray-900">Nombre de joueurs</label>
+                                        <input value={eventSelected.players}  onChange={(e) => setPlayers(e.target.value)} type="number" name="players" id="players"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:bg-white block w-full p-2.5"
+                                            required/>
+                                    </div>
+
+
+                                    <div className="w-full">
+                                        <label htmlFor="date"
+                                            className="block mb-2 text-sm font-medium text-gray-900">Date</label>
+                                        <input value={eventSelected?.date}  onChange={(e) => setDate(e.target.value)} type="date" name="date" id="date"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:bg-white block w-full p-2.5"
+                                            required/>
+                                    </div>
+
+                                    <div className="w-full">
+                                        <label htmlFor="time"
+                                            className="block mb-2 text-sm font-medium text-gray-900">Heure de début</label>
+                                        <input value={eventSelected?.time} onChange={(e) => setTime(e.target.value)} type="time" name="time" id="time"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:bg-white block w-full p-2.5"
+                                            required/>
+                                    </div>
+
+                                    {/* <div className="w-full">
+                                    <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900">Nom de l'entreprise</label>
+                                        <select onChange={ (e:any) => setCompany(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:bg-white block w-full p-2.5">
+                                    
+                                            {companies.map((company: any) => (
+                                                <option key={company.id} value={company.id}>{company.name}</option>
+                                            ))}
+
+                                        </select> 
+                                    </div> */}
+
+
+                            
+
+                                    <div className="sm:col-span-2">
+                                        <label htmlFor="description"
+                                            className="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                                        <textarea value={eventSelected?.description}  onChange={(e) => setDescription(e.target.value)} id="description" rows={6}
+                                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:bg-white"
+                                                placeholder="Description de l'évènement" required></textarea>
+                                    </div>
+                                </div>
+                            
+                                <button className="mt-3 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Ajouter</button>
+                            </form>
                             </Modal>
                             </>
                         ) : null}
