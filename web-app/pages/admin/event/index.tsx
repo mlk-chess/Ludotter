3e132a782aaff06f91c22de7c55f0f10b442aafd
jehanebnj <jehanebnj@gmail.com>
2023-06-29 17:03:04 +0200

    Fix status - FRONT

--- web-app/pages/admin/event/index.tsx
+++ web-app/pages/admin/event/index.tsx
@@ -11,7 +11,7 @@ interface Event {
     date: string;
     time: string;
     company:Company;
-    status:string;
+    status:number;
 }
 
 interface Company{
@@ -149,18 +149,30 @@ export default function Event() {
                                                     {event?.company?.name}
                                                 </td>
                                                  <td scope="row" className="px-6 py-3 text-gray-900">
-                                                   {event.status}
+                                                
+                                                    {event.status == -1 &&
+                                                        <span
+                                                            className="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-red-100">Annulé</span>
+                                                    }
+                                                    {event.status == 1 &&
+                                                        <span
+                                                            className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-green-100">Publié</span>
+                                                    }
                                                 </td>
-                                            
+                                                    
                                                 <td className="px-6 py-3 flex">
 
+                                                {event.status == 1 &&
                                                     <svg onClick={ () => openModal(event, true)} className="w-6 h-6 stroke-blue-500 cursor-pointer" fill="none" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                     </svg>
-
+                                                }
+                                                {event.status == 1 &&
                                                     <svg onClick={ () => openModal(event, false)}  fill="none" className="w-6 h-6 stroke-red-500 cursor-pointer" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                     </svg>
+                                                }
+                            
                                                 </td>
                                             </tr>
                                         )
