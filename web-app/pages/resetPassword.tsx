20979daad32579972e3575b2b81f236a3aa72ec4
jehanebnj <jehanebnj@gmail.com>
2023-07-01 00:00:05 +0200

    Add reset password

--- web-app/pages/resetPassword.tsx
+++ web-app/pages/resetPassword.tsx
@@ -1,24 +1,44 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import Link from "next/link";
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import Navbar from '@/components/home/Navbar';
-import { AuthError } from '@supabase/gotrue-js';
+import { useRouter } from 'next/router'
 
 
 export default function ResetPassword() {
   
-
-
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
     const supabaseClient = useSupabaseClient()
+    const router = useRouter();
+    
 
+   const resetPassword = useCallback(async  (e:any) => {
+
+    e.preventDefault();
+    if (password == confirmPassword) {
+
+        const { data, error } = await supabaseClient.auth.updateUser({
+            password: password
+        });
+
+        if (error) {
+            setError('Le lien de réinitialisation a expiré');
+        }else {
+            router.push('/')
+        }
+    }else{
+        setError('Les mots de passe ne correspondent pas');
+    }
+    
+    },[password, confirmPassword]);
 
     useEffect(() => {
         document.body.classList.add("bg-custom-light-orange");
-    });
+    },[]);
 
 
 
@@ -32,13 +52,31 @@ export default function ResetPassword() {
             </Head>
             <Navbar></Navbar>
             <main>
+
                 <div className="grid h-screen place-items-center">
+
+                {
+                        error !== "" ? 
+                        <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                <span className="sr-only">Error icon</span>
+                            </div>
+                            <div className="ml-3 text-sm font-normal">{error}</div>
+                            <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+                                <span className="sr-only">Close</span>
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </button>
+                        </div>
+                    
+                        : ""
+                    }   
                     <div
                         className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
                              <div className="flex justify-center mb-5">
                             <img src="./otter.png" alt="logo" className="w-20 h-20"/>
                         </div>
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={resetPassword}>
                             
                            <div>
                                 <label htmlFor="password"
