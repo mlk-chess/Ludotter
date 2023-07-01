d5b623696e1bf1e8099acde50d5eaa3ab8fb618f
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-01 12:00:38 +0200

    nest - user microservice controller

--- nest-app/user/src/app.controller.ts
+++ nest-app/user/src/app.controller.ts
@@ -11,4 +11,29 @@ export class AppController {
   getALlUsers() {
     return this.appService.getAllUsers();
   }
+
+  @MessagePattern({ cmd: 'users_getById' })
+  getUserById(id: string) {
+    return this.appService.getUserById(id);
+  }
+
+  @MessagePattern({ cmd: 'users_create' })
+  @UseFilters(new RpcValidationFilter())
+  @UsePipes(new ValidationPipe({ transform: true }))
+  createUser(newUser: any) {
+    return this.appService.createUser(newUser);
+  }
+
+  @MessagePattern({ cmd: 'users_update' })
+  @UseFilters(new RpcValidationFilter())
+  @UsePipes(new ValidationPipe({ transform: true }))
+  updateUser(user: any, id: string) {
+    return this.appService.updateUser(user, id);
+  }
+
+  @MessagePattern({ cmd: 'users_delete' })
+  deleteUser(id: string) {
+    return this.appService.deleteUser(id);
+  }
+
 }
