26e86381bfa34149084035552a22c29d8a6477f2
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-01 12:01:29 +0200

    Nest done for users

--- nest-app/api-gateway/src/user/user.controller.ts
+++ nest-app/api-gateway/src/user/user.controller.ts
@@ -7,8 +7,28 @@ export class UserController {
   constructor(@Inject('USER_SERVICE') private client: ClientProxy) { }
 
   @Get('all')
-  getParties() {
+  getUsers() {
     return this.client.send({ cmd: 'users_getAll' }, {});
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.client.send({ cmd: 'users_getById' }, id);
+  }
+
+  @Post('create')
+  createUser(@Body() newUser: any) {
+    return this.client.send({ cmd: 'users_create' }, newUser);
+  }
+
+  @Patch('update/:id')
+  updateUser(@Body() user: any, @Param('id') id: string) {
+    return this.client.send({ cmd: 'users_update' }, { user, id });
+  }
+
+  @Delete('delete/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.client.send({ cmd: 'users_delete' }, id);
   }
 
 }
