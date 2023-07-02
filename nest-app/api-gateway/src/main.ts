6e7fce71b155225b07d229d355240ccb90889438
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-02 20:10:47 +0200

    Reset

--- nest-app/api-gateway/src/main.ts
+++ nest-app/api-gateway/src/main.ts
@@ -5,7 +5,7 @@ import { json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: process.env.CORS || "http://localhost:3000",
     methods: "GET,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Accept, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials",
   });
