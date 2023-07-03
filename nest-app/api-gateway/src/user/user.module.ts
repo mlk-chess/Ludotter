d01904440e8afffec9f735b4d8748527d63e9c7d
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-03 10:14:41 +0200

    Guards and roles for users

--- nest-app/api-gateway/src/user/user.module.ts
+++ nest-app/api-gateway/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { UserController } from './user.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [UserController],
   providers: [{
     provide: 'USER_SERVICE',
