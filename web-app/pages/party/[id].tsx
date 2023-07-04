dca5680e15d278e85c0e1cbf56430bf36f7b6233
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-04 16:39:24 +0200

    Fix button design

--- web-app/pages/party/[id].tsx
+++ web-app/pages/party/[id].tsx
@@ -416,14 +416,11 @@ export default function Party() {
 
                                                 <div>
                                                     {participants && !participants.some(profile => profile.id === user?.id) ?
-                                                        <Button color="success" onClick={joinParty}>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-3">
-                                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                                    d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
-                                                            </svg>
+                                                        <button 
+                                                        className='px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-white bg-custom-orange rounded-lg hover:bg-custom-hover-orange' 
+                                                        onClick={joinParty}>
                                                             Rejoindre la fête
-                                                        </Button> :
+                                                        </button> :
 
                                                         <button
                                                             className='px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-white bg-custom-orange rounded-lg hover:bg-custom-hover-orange'
