e327344b6459c2caab744f862d46611af1404e97
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-05 16:53:51 +0200

    Test middleware

--- web-app/middleware.ts
+++ web-app/middleware.ts
@@ -11,53 +11,57 @@ export async function middleware(req: NextRequest) {
     
     if (session?.user) {
 
-        // let role = "";
-        // await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/me`, {
-        //     method: 'GET',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': 'Bearer ' + session?.access_token
-        //     }
-        // })
-        //     .then(response => response.json())
-        //     .then((data) => {
+        let role = "";
+        await fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + session?.access_token
+            }
+        })
+            .then(response => response.json())
+            .then((data) => {
+                console.log('get role')
 
-        //         role = data[0].role;
+                role = data[0].role;
                 
-        //     }).catch((error) => {
-        //     console.log(error);
-        // });
+            }).catch((error) => {
+            console.log(error);
+        });
+
+        console.log("verif")
             
-        // if (role == "ADMIN"){
-        //     if (req.nextUrl.pathname.startsWith('/admin')){
-        //         return res;
-        //     }
-        // }
+        if (role === "ADMIN"){
+            if (req.nextUrl.pathname.startsWith('/admin')){
+                return res;
+            }
+        }
 
-        // if (role == "COMPANY"){
-        //     if (
-        //         req.nextUrl.pathname.startsWith('/me/event') ||
-        //         req.nextUrl.pathname.startsWith('/profil')
-        //     ){
-        //         return res;
-        //     }
-        // }
+        if (role === "COMPANY"){
+            if (
+                req.nextUrl.pathname.startsWith('/me/event') ||
+                req.nextUrl.pathname.startsWith('/profil')
+            ){
+                return res;
+            }
+        }
 
-        // if (role == "CLIENT"){
-        //     if (
-        //         req.nextUrl.pathname.startsWith('/me/ordering') ||
-        //         req.nextUrl.pathname.startsWith('/me/announcement') ||
-        //         req.nextUrl.pathname.startsWith('/profil') ||
-        //         req.nextUrl.pathname.startsWith('/message')
+        if (role === "CLIENT"){
+            if (
+                req.nextUrl.pathname.startsWith('/me/ordering') ||
+                req.nextUrl.pathname.startsWith('/me/announcement') ||
+                req.nextUrl.pathname.startsWith('/profil') ||
+                req.nextUrl.pathname.startsWith('/message')
             
-        //     ){
-        //         return res;
-        //     }
-        // }
+            ){
+                return res;
+            }
+        }
 
-         return res
+        console.log("end redirect")
     }
 
+    console.log('redirect')
     const redirectUrl = req.nextUrl.clone()
     redirectUrl.pathname = '/'
     return NextResponse.redirect(redirectUrl)
