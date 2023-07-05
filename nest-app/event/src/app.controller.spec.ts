b3655a19eaa6b5ffbc081593b0e5633ad5116fa3
jehanebnj <jehanebnj@gmail.com>
2023-07-05 17:37:37 +0200

    Unit test for update an event

--- nest-app/event/src/app.controller.spec.ts
+++ nest-app/event/src/app.controller.spec.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { createEventDto } from './dto/create-event.dto';
 import { SupabaseService } from './supabase/supabase.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { updateEventDto } from './dto/update-event.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -17,6 +20,7 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
    describe('createEvent', () => {
@@ -30,8 +34,30 @@ describe('AppController', () => {
         companyId: 1,
         user: [{id:1}],
       };
-      const savedEvent = await appController.saveEvent(event);
-       expect(savedEvent).toEqual({ statusCode: 201, message: "Created" });
+      const result = await appController.saveEvent(event);
+      console.log(result);
+      expect(result).toEqual({ statusCode: 201, message: "Created" });
+    });
+  });
+  describe('updateEvent', () => {
+    it('should update an event', async () => {
+      const eventId = '31'; // ID de l'événement à mettre à jour
+      const updatedEvent: updateEventDto = {
+        id: eventId,
+        name: 'Loup garou',
+        description: 'Venez jouer au loup garou',
+        date: new Date('2024-01-02'),
+        time: '15:00',
+        players: 12,
+        user: [{id:1}],
+      };
+
+      jest.spyOn(appService, 'updateEvent').mockResolvedValue({ statusCode: 200, message: "Updated" });
+
+      const result = await appController.updateEvent(updatedEvent);
+
+      expect(result).toEqual({ statusCode: 200, message: "Updated" });
+      expect(appService.updateEvent).toHaveBeenCalledWith(updatedEvent);
     });
   });
 });
