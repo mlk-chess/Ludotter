cc9551ffbb036758dc1daec2edf396cb4343bf32
waseem1104 <waseem11@hotmail.fr>
2023-07-06 17:47:06 +0200

    fix category

--- web-app/pages/admin/category.tsx
+++ web-app/pages/admin/category.tsx
@@ -24,9 +24,8 @@ export default function Category() {
 
     const supabase = useSupabaseClient()
 
-    useEffect( () => {
-    document.body.classList.add("bg-custom-light-blue");
-    
+    const fetchData = async ()  => {
+
         fetch(`${process.env.NEXT_PUBLIC_CLIENT_API}/category`,{
             method:'GET',
         })
@@ -38,6 +37,12 @@ export default function Category() {
             console.log(error);
             
         });
+    }
+
+    useEffect( () => {
+    document.body.classList.add("bg-custom-light-blue");
+    
+        fetchData();
         
     },[]);
 
@@ -57,7 +62,7 @@ export default function Category() {
         })
         .then((response) => {
 
-            console.log(response)
+           
             return response.json()
         })
         .then( (data) => {
@@ -66,11 +71,12 @@ export default function Category() {
                 setSuccess("Catégorie créée.")
                 setError("")
             }else{
-                setError(data.response)
+                setError(data.response.message)
                 setSuccess("")
             }
 
             setShowModal(false);
+            fetchData();
          
         }).catch( (error) =>{
             console.log(error);  
@@ -97,6 +103,7 @@ export default function Category() {
             
             if (data.statusCode === 200){
                 setSuccess("Catégorie modifiée.")
+                fetchData();
                 setError("")
             }else{
                 setError(data.response.message)
@@ -133,6 +140,7 @@ export default function Category() {
                 setSuccess("")
             }
             setShowDeleteModal(false);
+            fetchData();
          
         }).catch( (error) =>{
             console.log(error);  
@@ -158,6 +166,15 @@ export default function Category() {
             <AdminLayout>
                 <div className="p-4 sm:ml-64">
                     <div className="p-4 mt-14">
+
+
+                            {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                    <span className="block sm:inline"> {error}</span>
+                                </div>}
+
+                            {success && <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                <span className="block sm:inline"> {success}</span>
+                            </div>}
 
                         {showModal ? (
                             <>
@@ -209,39 +226,9 @@ export default function Category() {
                             </>
                         ) : null}
 
-                    {
-                        success !== "" ? 
-                            <div id="toast-success" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
-                                <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg">
-                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                                    <span className="sr-only">Check icon</span>
-                                </div>
-                                <div className="ml-3 text-sm font-normal">{success}</div>
-                                <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8" data-dismiss-target="#toast-success" aria-label="Close">
-                                    <span className="sr-only">Close</span>
-                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                </button>
-                            </div>
-                        : ""
-                    }   
-
-                    {
-                        error !== "" ? 
-                        <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-                            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                <span className="sr-only">Error icon</span>
-                            </div>
-                            <div className="ml-3 text-sm font-normal">{error}</div>
-                            <button type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
-                                <span className="sr-only">Close</span>
-                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                            </button>
-                        </div>
-                    
-                        : ""
-                    }   
-                        <div className="flex justify-end">
+                   
+                
+                        <div className="flex justify-end mt-5">
                             <button onClick={() => setShowModal(true)} className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Créer une catégorie</button>
                         </div>
 
