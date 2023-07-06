4ea4d6c643933df491ac6aa8e506aab5415ba0c8
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-06 17:12:43 +0200

    Fix epreventdefault for form party new

--- web-app/pages/party/index.tsx
+++ web-app/pages/party/index.tsx
@@ -190,7 +190,6 @@ export default function New() {
                       :
 
                       <>
-                        {/* Add a button to link /party/new */}
                         <div className="flex flex-col items-center">
                           <h2 className=" text-3xl font-semibold">Créer une nouvelle soirée maintenant !</h2>
                           <Link href="/party/new"
