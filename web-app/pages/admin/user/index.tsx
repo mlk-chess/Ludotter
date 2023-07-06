173c88b0921af0b3fe263c761a666fc00c2dc328
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-06 20:21:57 +0200

    Link to new user admin

--- web-app/pages/admin/user/index.tsx
+++ web-app/pages/admin/user/index.tsx
@@ -187,7 +187,7 @@ export default function User() {
                         }
 
                         <div className="flex justify-end">
-                            <button className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Ajouter un utilisateur</button>
+                            <a href='user/new' className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Ajouter un utilisateur</a>
                         </div>
 
                         <div className="relative overflow-x-auto mt-5">
